47c6a0320df2362158d60067ce6037fc33f136cb
bartek <bartamalio@gmail.com>
2026-01-15 16:11:51 +0100

    dokumentacja cd

--- widoki.sql
+++ widoki.sql
@@ -1,24 +1,29 @@
--- Widok 1: Gracze z drużynami
-CREATE VIEW widok_gracze_z_druzyna AS
-SELECT g.id_gracza, g.imie, g.nazwisko, g.pozycja, z.nazwa AS nazwa_druzyny
-FROM gracz g
-LEFT JOIN zespol z ON g.id_zespolu = z.id_zespolu;
+-- Widok 1: Zawodnicy z zespołem
+CREATE OR REPLACE VIEW widok_zawodnicy_z_zespolem AS
+SELECT
+  z.id_zawodnika,
+  z.imie,
+  z.nazwisko,
+  z.pozycja,
+  zes.nazwa AS nazwa_zespolu
+FROM zawodnik z
+LEFT JOIN zespol zes ON z.id_zespolu = zes.id_zespolu;
 
--- Widok 2: Średnie statystyki
-CREATE VIEW widok_srednie_statystyki_gracza AS
-SELECT 
-  g.id_gracza,
-  g.imie || ' ' || g.nazwisko AS gracz,
+-- Widok 2: Średnie statystyki zawodnika
+CREATE OR REPLACE VIEW widok_srednie_statystyki_zawodnika AS
+SELECT
+  z.id_zawodnika,
+  z.imie || ' ' || z.nazwisko AS zawodnik_nazwa,
   ROUND(AVG(s.punkty)::numeric,2) AS srednie_punkty,
   ROUND(AVG(s.asysty)::numeric,2) AS srednie_asysty,
   ROUND(AVG(s.zbiorki)::numeric,2) AS srednie_zbiorki
-FROM gracz g
-JOIN statystyki_meczu s ON g.id_gracza = s.id_gracza
-GROUP BY g.id_gracza, g.imie, g.nazwisko;
+FROM zawodnik z
+JOIN statystyki_meczu s ON z.id_zawodnika = s.id_zawodnika
+GROUP BY z.id_zawodnika, z.imie, z.nazwisko;
 
 -- Widok 3: Wyniki meczów
-CREATE VIEW widok_wyniki_meczy AS
-SELECT m.id_meczu, m.data_meczu, 
+CREATE OR REPLACE VIEW widok_wyniki_meczy AS
+SELECT m.id_meczu, m.data_meczu,
   g.nazwa AS gospodarz, m.wynik_gospodarza,
   z.nazwa AS gosc, m.wynik_goscia
 FROM mecz m
