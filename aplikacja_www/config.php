85c0745e941515417c693c7d6821386020175e74
bartek <bartamalio@gmail.com>
2026-01-28 09:54:03 +0100

    render.yaml added

--- aplikacja_www/config.php
+++ aplikacja_www/config.php
@@ -1,12 +1,14 @@
 <?php
+// Pobieranie danych dostępowych do bazy danych z zmiennych środowiskowych.
+// Używamy wartości domyślnych dla środowiska lokalnego, jeśli zmienne środowiskowe nie są ustawione.
 
-// Plik konfiguracyjny bazy danych
-// Uzupełnij poniższe dane swoimi danymi dostępowymi do bazy PostgreSQL
+$db_host = getenv('DB_HOST') ?: 'localhost';      // Adres serwera bazy danych
+$db_port = getenv('DB_PORT') ?: '5432';           // Port serwera bazy danych
+$db_name = getenv('DB_NAME') ?: 'nba_db';         // Nazwa bazy danych
+$db_user = getenv('DB_USER') ?: 'bartek';         // Nazwa użytkownika bazy danych
+$db_pass = getenv('DB_PASS') ?: 'lebron';         // Hasło użytkownika bazy danych
 
-$db_host = 'localhost';      // Adres serwera bazy danych
-$db_port = '5432';           // Port serwera bazy danych
-$db_name = 'nba_db';     // Nazwa bazy danych
-$db_user = 'bartek'; // Nazwa użytkownika bazy danych
-$db_pass = 'lebron';      // Hasło użytkownika bazy danych
-
+// Możesz dodać inne zmienne konfiguracyjne tutaj, jeśli są potrzebne.
+// Na przykład:
+// $app_debug = getenv('APP_DEBUG') ?: false;
 ?>
